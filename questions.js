e77e66dc1f994a0067d5bcbd9c360263cf8017f3
Tarnof <tarnof@hotmail.com>
2023-07-04 10:54:32 +0200

    modif page question

--- questions.js
+++ questions.js
@@ -1,22 +1,12 @@
+//Question aléatoire
 function selectRandomQuestion(questions) {
     const randomIndex = Math.floor(Math.random() * questions.length);
     return questions[randomIndex];
 }
 
 
-async function getQuestions() {
-    let brutResponse = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1Hc7PM4OhE20N9kdYDjNrgP80XtskSrTsSA39Lw4SoNo/values/Questions!A1:G500?key=AIzaSyBkIWqYfn-E9ccj9fevEihFr7EBey3dap8");
-    let response = await brutResponse.json();
-    response.values.sort((a, b) => a[0].localeCompare(b[0]));
-
-    const randomQuestion = selectRandomQuestion(response.values);
-    const randomQuestionHTML = generateQuestionHTML(randomQuestion);
-
-    document.getElementById("questionsContent").innerHTML = randomQuestionHTML;
-}
-
-function generateQuestionHTML(question) {
-    let questionHTML = '<div class="accordion-item">' +
+function generateRandomQuestionHTML(question) {
+    let randomQuestionHTML = '<div class="accordion-item">' +
         '<h2 class="accordion-header">' +
         '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">' +
         '<strong>' + question[0] + '</strong>' +
@@ -25,17 +15,76 @@ function generateQuestionHTML(question) {
         '<div class="accordion-body">';
 
     for (let i = 1; i < question.length; i++) {
-        questionHTML += '<p>' + question[i] + '</p>';
+        randomQuestionHTML += '<p>' + question[i] + '</p>';
     }
+    randomQuestionHTML += '</div></div></div>';
 
-    '<p>' + question[1] + '</p>' +
-        '</div></div></div>';
-
-    return questionHTML;
+    return randomQuestionHTML;
 }
 
 
+
+async function getRandomQuestions() {
+    let brutResponse = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1Hc7PM4OhE20N9kdYDjNrgP80XtskSrTsSA39Lw4SoNo/values/Questions!A1:G500?key=" + API_KEY);
+    let response = await brutResponse.json();
+    response.values.sort((a, b) => a[0].localeCompare(b[0]));
+
+    const randomQuestion = selectRandomQuestion(response.values);
+    const randomQuestionHTML = generateRandomQuestionHTML(randomQuestion);
+
+    document.getElementById("randomQuestionsContent").innerHTML = randomQuestionHTML;
+}
+
+var questionsWrap = document.getElementById('questionsWrap');
+var computedStyles = getComputedStyle(questionsWrap);
+
+var randomQuestionsWrap = document.getElementById('randomQuestionsWrap');
 
 function refreshQuestion() {
-    getQuestions();
+    if (computedStyles.display !== 'none') {
+        randomQuestionsWrap.style.display = 'block'
+        questionsWrap.style.display = 'none';
+
+    }
+    getRandomQuestions();
 }
+
+function afficherQuestions() {
+    questionsWrap.style.display = 'block';
+    randomQuestionsWrap.style.display = 'none'
+}
+//Question aléatoire
+
+
+
+//Toutes les questions
+async function getQuestions() {
+    let brutResponse = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1Hc7PM4OhE20N9kdYDjNrgP80XtskSrTsSA39Lw4SoNo/values/Questions!A1:G500?key=" + API_KEY
+    );
+
+    let response = await brutResponse.json();
+
+    response.values.sort((a, b) => a[0].localeCompare(b[0]));
+
+    let questionsContentHTML = "";
+
+    for (let i = 0; i < response.values.length; i++) {
+        questionsContentHTML += '<div class="accordion-item">';
+        questionsContentHTML += '<h2 class="accordion-header" id="heading' + i + '">';
+        questionsContentHTML += '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' + i + '" aria-expanded="false" aria-controls="collapse' + i + '">';
+        questionsContentHTML += '<strong>' + response.values[i][0] + '</strong>';
+        questionsContentHTML += '</button></h2>';
+        questionsContentHTML += '<div id="collapse' + i + '" class="accordion-collapse collapse" aria-labelledby="heading' + i + '" data-bs-parent="#notionsContent">';
+        questionsContentHTML += '<div class="accordion-body">';
+        for (let j = 1; j < response.values[i].length; j++) {
+            questionsContentHTML += '<p>' + response.values[i][j] + '</p>';
+        }
+        questionsContentHTML += '</div></div></div>';
+    }
+
+    document.getElementById("questionsContent").innerHTML = questionsContentHTML;
+
+};
+
+getQuestions();
+//Toutes les questions
